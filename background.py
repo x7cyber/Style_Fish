a0102af975815830dd7a2ec09ccf0f80ac2b5a1e
M-Cyber7 <150235153+M-Cyber7@users.noreply.github.com>
2024-04-19 11:15:23 +0700

    Add files via upload

---
+++ background.py
@@ -0,0 +1,6 @@
+# Kursor dan background ada disini!
+
+import os
+
+os.system('cp Data/colors.properties /data/data/com.termux/files/home/.termux')
+os.system('cp Data/font.ttf /data/data/com.termux/files/home/.termux')
